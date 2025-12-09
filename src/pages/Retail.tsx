d184f7b438fd1d0fa2892413bc9433ae6ae1b386
Jafry Deep <deepjafry@gmail.com>
2025-12-09 20:47:03 +0600

    changes updated

--- src/pages/Retail.tsx
+++ src/pages/Retail.tsx
@@ -3,10 +3,6 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { EnquireSidebar } from "@/components/EnquireSidebar";
 
-import retail1 from "@/assets/retail-1.jpg";
-import retail2 from "@/assets/retail-2.jpg";
-import retail3 from "@/assets/retail-3.jpg";
-
 interface RetailProject {
   id: string;
   title: string;
@@ -15,41 +11,67 @@ interface RetailProject {
 }
 
 const projects: RetailProject[] = [
-  {
-    id: "1",
-    title: "The Collection at UB City",
-    description: "India's first Luxury Shopping Mall",
-    image: retail1,
-  },
-  {
-    id: "2",
-    title: "Forum South Bengaluru",
-    description: "Bangalore",
-    image: retail2,
-  },
+  // Hospitality projects
   {
     id: "3",
-    title: "Forum Rex Walk",
-    description: "Bangalore",
-    image: retail3,
+    title: "Sheraton Grand",
+    description: "Hotel & Convention Center at Whitefield",
+    image: "https://prestigecorporatesite.s3.ap-south-1.amazonaws.com/projectimage/de91e3b0-447f-4279-a0d9-e90fe3f8f138.webp",
   },
   {
     id: "4",
-    title: "Forum Neighbourhood Mall",
-    description: "Whitefield, Bangalore",
-    image: retail1,
-  },
-  {
-    id: "5",
-    title: "Forum Mall Koramangala",
-    description: "Koramangala, Bangalore",
-    image: retail2,
+    title: "Bayfront Suites",
+    description: "Spa & Resort in Yelahanka",
+    image: "https://prestigecorporatesite.s3.ap-south-1.amazonaws.com/projectimage/042e94e1-5982-4266-9000-148d8411988d.webp",
   },
   {
     id: "6",
-    title: "Forum Sujana Mall",
-    description: "Hyderabad",
-    image: retail3,
+    title: "JW Marriott Bengaluru Golfshire",
+    description: "Resort & Spa in Devanahalli",
+    image: "https://prestigecorporatesite.s3.ap-south-1.amazonaws.com/projectimage/537a53f8-4a7d-474f-b633-fef4c8261733.webp",
+  },
+  {
+    id: "9",
+    title: "Resort & Spa in Goa",
+    description: "Luxury Resort & Spa",
+    image: "https://prestigecorporatesite.s3.ap-south-1.amazonaws.com/projectimage/c74734e2-a56f-4b4c-a2db-4c1b144e8cfc.webp",
+  },
+  {
+    id: "10",
+    title: "Golfshire Club",
+    description: "18 Hole Championship Golf Course",
+    image: "https://prestigecorporatesite.s3.ap-south-1.amazonaws.com/projectimage/20d19d30-399b-4273-a5ba-9ef009e97f1c.webp",
+  },
+  {
+    id: "11",
+    title: "ITC Taj",
+    description: "A Luxury Collection Resort & Spa",
+    image: "https://www.itchotels.com/content/dam/itchotels/in/umbrella/itc/hotels/itcmughal-agra/images/overview-landing-page/overview/desktop/spa-pool.png",
+  },
+  {
+    id: "12",
+    title: "ITC Collection",
+    description: "A collection of luxury hotels and resorts",
+    image: "https://static.businessworld.in/1538135698_xkt4dU_cropped_2_.png",
+  },
+  {
+    id: "13",
+    title: "Meridian Gulf",
+    description: "Luxury and Prestige Outlets",
+    image: "https://www.modabahrain.com/wp-content/uploads/2024/04/Prestige-Outlets-1024x768.jpg",
+  },
+  // Retail projects
+  {
+    id: "14",
+    title: "The Collection",
+    description: "India's first Luxury Shopping Mall",
+    image: "https://prestigecorporatesite.s3.ap-south-1.amazonaws.com/projectimage/0eb35362-ddfc-41f5-b461-2403c5d5c2ec.webp",
+  },
+  {
+    id: "15",
+    title: "Forum",
+    description: "Premium Shopping Destination",
+    image: "https://prestigecorporatesite.s3.ap-south-1.amazonaws.com/projectimage/a4ca9e96-6941-4581-8680-4ce77b7cc0eb.webp",
   },
 ];
 
